984e3e809861fc2283c5fb0e5eb46747727bfcad
Urmat Muratov <muraurm@gmail.com>
2023-10-09 14:44:18 +0600

    create Event

--- src/Components/CreateEvent/index.jsx
+++ src/Components/CreateEvent/index.jsx
@@ -2,31 +2,17 @@ import React, { useState } from "react";
 import API from "../../requester";
 import { useSelector } from "react-redux";
 import { selectIsAuthenticated } from "../Redux/isAuthentificatedSlice";
-import Cookies from "js-cookie";
+import { useCookies } from "react-cookie"; // Verwende react-cookie
 
 function CreateEventForm() {
-  const accessToken = Cookies.get("accessToken").replace("Bearer ", "");
-  const headers = {
-    Authorization: `Bearer ${accessToken}`,
-  };
+  const isAuthenticated = useSelector(selectIsAuthenticated);
 
   const [eventData, setEventData] = useState({
     title: "",
     description: "",
-    location: {
-      // Ort-Daten
-      city: "",
-      country: "",
-    },
-    can_subscribe: "open", // Standardwert für "can_subscribe"
-    is_free: false, // Standardwert auf false setzen
-    tags: [], // Ein leeres Array für Tags
-    img: "", // Bild-URL
-    start_event_date: "", // Leerer String für das Startdatum
-    end_event_date: "", // Leerer String für das Enddatum
-    limit_of_followers: null, // null für "limit_of_followers"
   });
-  const isAuthenticated = useSelector(selectIsAuthenticated);
+
+  const [cookies] = useCookies(["accessToken"]); // Verwende react-cookie, um auf den AccessToken-Cookie zuzugreifen
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -40,7 +26,29 @@ function CreateEventForm() {
     e.preventDefault();
 
     try {
-      const response = await API.post("/events/", eventData, { headers });
+      // Den AccessToken aus den Cookies abrufen
+      const accessToken = cookies.accessToken || "";
+
+      // Überprüfen, ob der Benutzer authentifiziert ist
+      if (!isAuthenticated || !accessToken) {
+        // Zeige eine Meldung oder blockiere den Zugriff
+        console.log("Du bist nicht angemeldet.");
+        return;
+      }
+
+      // Hier kannst du deine API-Anfrage mit den Headers senden, die nur title und description enthalten
+      const headers = {
+        Authorization: `Bearer ${accessToken}`,
+      };
+      const response = await API.post(
+        "/events/",
+        {
+          title: eventData.title,
+          description: eventData.description,
+        },
+        { headers }
+      );
+
       const createdEvent = response.data; // Hier erhältst du die erstellte Veranstaltung
       console.log("Veranstaltung erstellt:", createdEvent);
     } catch (error) {
@@ -55,7 +63,7 @@ function CreateEventForm() {
 
   return (
     <div>
-      <h1>Veranstaltung erstellen</h1>
+      <h1>Event create</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Titel:
@@ -69,109 +77,12 @@ function CreateEventForm() {
         </label>
         <br />
         <label>
-          Beschreibung:
+          description:
           <textarea
             name="description"
             value={eventData.description}
             onChange={handleInputChange}
             required
-          />
-        </label>
-        <br />
-        <label>
-          Stadt:
-          <input
-            type="text"
-            name="location.city"
-            value={eventData.location.city}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Land:
-          <input
-            type="text"
-            name="location.country"
-            value={eventData.location.country}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Kann abonniert werden:
-          <select
-            name="can_subscribe"
-            value={eventData.can_subscribe}
-            onChange={handleInputChange}
-          >
-            <option value="open">Ja</option>
-            <option value="closed">Nein</option>
-            <option value="restricted">Beschränkt</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Kostenlos:
-          <input
-            type="checkbox"
-            name="is_free"
-            checked={eventData.is_free}
-            onChange={(e) =>
-              setEventData({ ...eventData, is_free: e.target.checked })
-            }
-          />
-        </label>
-        <br />
-        <label>
-          Tags (durch Kommas getrennt):
-          <input
-            type="text"
-            name="tags"
-            value={eventData.tags.join(", ")}
-            onChange={(e) =>
-              setEventData({ ...eventData, tags: e.target.value.split(", ") })
-            }
-          />
-        </label>
-        <br />
-        <label>
-          Bild-URL:
-          <input
-            type="text"
-            name="img"
-            value={eventData.img}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Startdatum:
-          <input
-            type="date"
-            name="start_event_date"
-            value={eventData.start_event_date}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Enddatum:
-          <input
-            type="date"
-            name="end_event_date"
-            value={eventData.end_event_date}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Maximale Anzahl von Followern:
-          <input
-            type="number"
-            name="limit_of_followers"
-            value={eventData.limit_of_followers}
-            onChange={handleInputChange}
           />
         </label>
         <br />
